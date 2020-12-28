b48cea89c46411f924fe9aba66d130d37884361b
MrTaufiq94 <m.taufiqfariduddin94@gmail.com>
2020-12-28 14:45:16 +0800

    Day 5 : install and use fontawesome using npm

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,7 +59,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                                    <i class="fas fa-user"></i> {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
